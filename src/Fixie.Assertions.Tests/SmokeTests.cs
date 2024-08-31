c1555cefc886ff2c237f8e5881ca8597a2964d4f
Patrick Lioi <patrick.lioi@gmail.com>
2024-08-30 21:31:09 -0500

    Remove failing smoke test now that continuous integration builds correctly detect and report such a failure.

--- src/Fixie.Assertions.Tests/SmokeTests.cs
+++ src/Fixie.Assertions.Tests/SmokeTests.cs
@@ -5,9 +5,4 @@ public class SmokeTests
     public void Pass()
     {
     }
-
-    public void Fail()
-    {
-        throw new Exception("This test is written to fail, to demonstrate failure reporting.");
-    }
 }
