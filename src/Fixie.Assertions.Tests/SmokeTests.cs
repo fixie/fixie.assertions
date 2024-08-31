e503082bb71c864ee38ccf1301a2f450320323ee
Patrick Lioi <patrick.lioi@gmail.com>
2024-08-30 21:21:14 -0500

    Smoke tests demonstrate the running of tests and detection of failures during automated builds.

---
+++ src/Fixie.Assertions.Tests/SmokeTests.cs
@@ -0,0 +1,13 @@
+﻿namespace Fixie.Assertions.Tests;
+
+public class SmokeTests
+{
+    public void Pass()
+    {
+    }
+
+    public void Fail()
+    {
+        throw new Exception("This test is written to fail, to demonstrate failure reporting.");
+    }
+}
