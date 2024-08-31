27617c5f88240f7baf7c7dc4e7980c02c0e6d9b4
Patrick Lioi <patrick.lioi@gmail.com>
2024-08-31 17:53:57 -0500

    The build script temporarily evaluates package related MSBuild properties, enabling feedback about packaging format/content and Deterministic Builds.

--- build.ps1
+++ build.ps1
@@ -8,6 +8,30 @@ function step($command) {
     if ($lastexitcode -ne 0) { throw $lastexitcode }
 }
 
+function analyze-msbuild-properties {
+    $projects = ("Fixie.Assertions", "Fixie.Assertions.Tests")
+    $properties = (
+        "Deterministic",
+        "ContinuousIntegrationBuild",
+        "EmbedUntrackedSources",
+        "DebugType",
+        "IncludeSymbols",
+        "SymbolPackageFormat"
+        )
+
+    foreach ($project in $projects) {
+        write-host "=============================================="
+        write-host "Analyzing $project Properties"
+        write-host
+    
+        foreach ($property in $properties) {
+            write-host $property
+            dotnet build src/$project -c Release --nologo --getProperty:$property
+            write-host
+        }
+    }
+}
+
 if (test-path packages) { remove-item packages -Recurse }
 
 step { dotnet tool restore }
@@ -17,6 +41,8 @@ step { dotnet fixie *.Tests -c Release --no-build }
 
 if ($pack) {
     step { dotnet pack src/Fixie.Assertions -o packages -c Release --no-build --nologo }
+
+    analyze-msbuild-properties
 
     step { dotnet tool install dotnet-validate --version 0.0.1-preview.304  }
     $packages = get-childitem packages/*.nupkg
