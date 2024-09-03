d877c17039d9a0e393e19d2022d5532daf169c55
Patrick Lioi <patrick.lioi@gmail.com>
2024-09-03 18:03:18 -0500

    The publish script can signal a warning state when development build publishing is skipped for a logged reason.

--- publish.ps1
+++ publish.ps1
@@ -1,8 +1,17 @@
+param([switch]$ci)
+
+$ErrorActionPreference = "Stop"
+
 $packages = get-childitem packages/*.nupkg
 
 if ([string]::IsNullOrEmpty($env:PACKAGE_API_KEY)) {
     $severity = "::error::"
     $exitCode = 1;
+
+    if ($ci) {
+        $severity = "::warning::"
+        $exitCode = 0;
+    }
 
     Write-Host "$($severity)$($MyInvocation.MyCommand.Name): PACKAGE_API_KEY is empty or not set. No packages will be pushed."
 
