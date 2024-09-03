31c5a85233c71dbf8f3e55f3d4766be8c39774d2
Patrick Lioi <patrick.lioi@gmail.com>
2024-09-03 17:40:42 -0500

    The publish script can signal an error state by default when publishing is skipped for a logged reason.

--- publish.ps1
+++ publish.ps1
@@ -1,7 +1,12 @@
-﻿$packages = get-childitem packages/*.nupkg
+$packages = get-childitem packages/*.nupkg
 
 if ([string]::IsNullOrEmpty($env:PACKAGE_API_KEY)) {
-    Write-Host "$($MyInvocation.MyCommand.Name): PACKAGE_API_KEY is empty or not set. No packages will be pushed."
+    $severity = "::error::"
+    $exitCode = 1;
+
+    Write-Host "$($severity)$($MyInvocation.MyCommand.Name): PACKAGE_API_KEY is empty or not set. No packages will be pushed."
+
+    exit $exitCode
 } else {
     foreach ($package in $packages) {
         dotnet nuget push $package --source $env:PACKAGE_URL --api-key $env:PACKAGE_API_KEY
