e7e3051387d2d5c6d6ed5301831e533d9dadd5cc
Patrick Lioi <patrick.lioi@gmail.com>
2024-09-11 21:30:53 -0500

    Reorder members for top-down readability.

--- src/Fixie.Assertions/Serialization.cs
+++ src/Fixie.Assertions/Serialization.cs
@@ -5,30 +5,26 @@ namespace Fixie.Assertions;
 
 static class Serialization
 {
-    static string Serialize(bool x) => x ? "true" : "false";
+    public static string Serialize<T>(T any)
+    {
+        if (any == null) return "null";
 
-    static string Serialize(Type x) =>
-        $"typeof({x switch
-        {
-            _ when x == typeof(bool) => "bool",
-            _ when x == typeof(sbyte) => "sbyte",
-            _ when x == typeof(byte) => "byte",
-            _ when x == typeof(short) => "short",
-            _ when x == typeof(ushort) => "ushort",
-            _ when x == typeof(int) => "int",
-            _ when x == typeof(uint) => "uint",
-            _ when x == typeof(long) => "long",
-            _ when x == typeof(ulong) => "ulong",
-            _ when x == typeof(nint) => "nint",
-            _ when x == typeof(nuint) => "nuint",
-            _ when x == typeof(decimal) => "decimal",
-            _ when x == typeof(double) => "double",
-            _ when x == typeof(float) => "float",
-            _ when x == typeof(char) => "char",
-            _ when x == typeof(string) => "string",
-            _ when x == typeof(object) => "object",
-            _ => x.ToString()
-        }})";
+        if (typeof(T) == typeof(bool))
+            return Serialize((bool)(object)any);
+
+        if (typeof(T) == typeof(char))
+            return Serialize((char)(object)any);
+
+        if (typeof(T) == typeof(string))
+            return Serialize((string)(object)any);
+
+        if (typeof(T) == typeof(Type))
+            return Serialize((Type)(object)any);
+
+        return any.ToString() ?? any.GetType().ToString();
+    }
+
+    static string Serialize(bool x) => x ? "true" : "false";
 
     static string Serialize(char x) => $"'{Escape(x)}'";
 
@@ -85,29 +81,33 @@ static class Serialization
             _ => x.ToString()
         };
 
+    static string Serialize(Type x) =>
+        $"typeof({x switch
+        {
+            _ when x == typeof(bool) => "bool",
+            _ when x == typeof(sbyte) => "sbyte",
+            _ when x == typeof(byte) => "byte",
+            _ when x == typeof(short) => "short",
+            _ when x == typeof(ushort) => "ushort",
+            _ when x == typeof(int) => "int",
+            _ when x == typeof(uint) => "uint",
+            _ when x == typeof(long) => "long",
+            _ when x == typeof(ulong) => "ulong",
+            _ when x == typeof(nint) => "nint",
+            _ when x == typeof(nuint) => "nuint",
+            _ when x == typeof(decimal) => "decimal",
+            _ when x == typeof(double) => "double",
+            _ when x == typeof(float) => "float",
+            _ when x == typeof(char) => "char",
+            _ when x == typeof(string) => "string",
+            _ when x == typeof(object) => "object",
+            _ => x.ToString()
+        }})";
+
     public static string SerializeList<T>(T[] items)
     {
         var formattedItems = string.Join("," + NewLine, items.Select(arg => "    " + Serialize(arg)));
 
         return $"[{NewLine}{formattedItems}{NewLine}]";
-    }
-
-    public static string Serialize<T>(T any)
-    {
-        if (any == null) return "null";
-
-        if (typeof(T) == typeof(bool))
-            return Serialize((bool)(object)any);
-
-        if (typeof(T) == typeof(char))
-            return Serialize((char)(object)any);
-
-        if (typeof(T) == typeof(string))
-            return Serialize((string)(object)any);
-
-        if (typeof(T) == typeof(Type))
-            return Serialize((Type)(object)any);
-
-        return any.ToString() ?? any.GetType().ToString();
     }
 }
