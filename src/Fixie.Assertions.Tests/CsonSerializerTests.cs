e5ea41cd2f9cd4b1511b41a6200842d0ca6be008
Patrick Lioi <patrick.lioi@gmail.com>
2024-09-20 13:20:40 -0500

    Split `[...]` list CsonSerializerTests to their own dedicated test class, so that it can grow to better demonstrate the full scope of their handling of various types.

--- src/Fixie.Assertions.Tests/CsonSerializerTests.cs
+++ src/Fixie.Assertions.Tests/CsonSerializerTests.cs
@@ -437,33 +437,6 @@ class CsonSerializerTests
         Serialize(typeof(int?)).ShouldBe("typeof(int?)");
     }
 
-    public void ShouldSerializeLists()
-    {
-        Serialize((int[]?)null)
-            .ShouldBe("null");
-
-        Serialize((int[])[])
-            .ShouldBe("[]");
-
-        Serialize((int[])[1, 2, 3])
-            .ShouldBe("""
-                      [
-                        1,
-                        2,
-                        3
-                      ]
-                      """);
-
-        //TODO: The indentation of lists became incorrect once the contained item type, in this case
-        //      strings, started to be written useing WriteRawValue. Apparently the preceding expected
-        //      whitespace gets skipped as a consequence of WriteRawValue.
-        Serialize((List<string>)["ABC", "123"])
-            .ShouldBe("""
-                      ["ABC","123"
-                      ]
-                      """);
-    }
-
     public void ShouldSerializeObjectProperties()
     {
         Serialize((object?)null)
@@ -538,7 +511,7 @@ class CsonSerializerTests
                       """);
     }
 
-    public void ShouldSerializingAmbiguouslyUninterestingObjects()
+    public void ShouldSerializeAmbiguouslyUninterestingObjects()
     {
         Serialize(new object())
             .ShouldBe("{}");
