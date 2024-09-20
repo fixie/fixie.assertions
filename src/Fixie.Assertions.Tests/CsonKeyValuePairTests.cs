c9737ad81c61b33530d5b4e1be1578aa242104ad
Patrick Lioi <patrick.lioi@gmail.com>
2024-09-20 13:24:01 -0500

    Split `{...}` key/value pair collection CsonSerializerTests to their own dedicated test class, so that it can grow to better demonstrate the full scope of their handling of various types.

---
+++ src/Fixie.Assertions.Tests/CsonKeyValuePairTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Tests;
+
+class CsonPropertyValuePairTests
+{
+    public void ShouldSerializeDictionaries()
+    {
+        Dictionary<string, object>? stringKeys = null;
+
+        Serialize(stringKeys)
+            .ShouldBe("null");
+
+        stringKeys = new Dictionary<string, object>
+        {
+            { "First Key", "First Value" },
+            { "Second Key", "Second Value" },
+            { "Third Key", "Third Value" }
+        };
+
+        Serialize(stringKeys)
+            .ShouldBe("""
+                      {
+                        "First Key": "First Value",
+                        "Second Key": "Second Value",
+                        "Third Key": "Third Value"
+                      }
+                      """);
+
+        Dictionary<int, object>? numericKeys = null;
+
+        Serialize(numericKeys)
+            .ShouldBe("null");
+
+        numericKeys = new Dictionary<int, object>
+        {
+            { 1, "First Value" },
+            { 2, "Second Value" },
+            { 3, "Third Value" }
+        };
+
+        Serialize(numericKeys)
+            .ShouldBe("""
+                      {
+                        "1": "First Value",
+                        "2": "Second Value",
+                        "3": "Third Value"
+                      }
+                      """);
+    }
+
+    static string Serialize<T>(T value)
+        => CsonSerializer.Serialize(value);
+}
--- src/Fixie.Assertions.Tests/CsonSerializerTests.cs
+++ src/Fixie.Assertions.Tests/CsonSerializerTests.cs
@@ -466,51 +466,6 @@ class CsonSerializerTests
                       """);
     }
 
-    public void ShouldSerializeDictionaries()
-    {
-        Dictionary<string, object>? stringKeys = null;
-
-        Serialize(stringKeys)
-            .ShouldBe("null");
-
-        stringKeys = new Dictionary<string, object>
-        {
-            { "First Key", "First Value" },
-            { "Second Key", "Second Value" },
-            { "Third Key", "Third Value" }
-        };
-
-        Serialize(stringKeys)
-            .ShouldBe("""
-                      {
-                        "First Key": "First Value",
-                        "Second Key": "Second Value",
-                        "Third Key": "Third Value"
-                      }
-                      """);
-
-        Dictionary<int, object>? numericKeys = null;
-
-        Serialize(numericKeys)
-            .ShouldBe("null");
-
-        numericKeys = new Dictionary<int, object>
-        {
-            { 1, "First Value" },
-            { 2, "Second Value" },
-            { 3, "Third Value" }
-        };
-
-        Serialize(numericKeys)
-            .ShouldBe("""
-                      {
-                        "1": "First Value",
-                        "2": "Second Value",
-                        "3": "Third Value"
-                      }
-                      """);
-    }
-
     public void ShouldSerializeAmbiguouslyUninterestingObjects()
     {
         Serialize(new object())
