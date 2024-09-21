8909015aeccf47af30d44d4f1ed034488d8f23f8
Patrick Lioi <patrick.lioi@gmail.com>
2024-09-20 22:35:01 -0500

    Split numeric CsonScalarTests to their own dedicated test class, expanding coverage to demonstrate that `Nullable<>` variations of the primitive numeric types serialize as expected in both the null and unwrapping-non-null cases.

--- src/Fixie.Assertions.Tests/CsonScalarTests.cs
+++ src/Fixie.Assertions.Tests/CsonScalarTests.cs
@@ -10,64 +10,6 @@ class CsonScalarTests
         Serialize(false).ShouldBe("false");
     }
 
-    public void ShouldSerializeIntegralNumbers()
-    {
-        Serialize(sbyte.MinValue).ShouldBe("-128");
-        Serialize(sbyte.MaxValue).ShouldBe("127");
-        
-        Serialize(byte.MinValue).ShouldBe("0");
-        Serialize(byte.MaxValue).ShouldBe("255");
-        
-        Serialize(short.MinValue).ShouldBe("-32768");
-        Serialize(short.MaxValue).ShouldBe("32767");
-        
-        Serialize(ushort.MinValue).ShouldBe("0");
-        Serialize(ushort.MaxValue).ShouldBe("65535");
-
-        Serialize(int.MinValue).ShouldBe("-2147483648");
-        Serialize(int.MaxValue).ShouldBe("2147483647");
-
-        Serialize(uint.MinValue).ShouldBe("0");
-        Serialize(uint.MaxValue).ShouldBe("4294967295");
-        
-        Serialize(long.MinValue).ShouldBe("-9223372036854775808");
-        Serialize(long.MaxValue).ShouldBe("9223372036854775807");
-
-        Serialize(ulong.MinValue).ShouldBe("0");
-        Serialize(ulong.MaxValue).ShouldBe("18446744073709551615");
-
-        Serialize((nint)(-1)).ShouldBe("-1");
-        Serialize((nint)0).ShouldBe("0");
-        Serialize((nint)1).ShouldBe("1");
-
-        Serialize((nuint)0).ShouldBe("0");
-        Serialize((nuint)1).ShouldBe("1");
-    }
-
-    public void ShouldSerializeFractionalNumbers()
-    {
-        Serialize(decimal.MinValue).ShouldBe("-79228162514264337593543950335");
-        Serialize(0m).ShouldBe("0");
-        Serialize(1m).ShouldBe("1");
-        Serialize(0.1m).ShouldBe("0.1");
-        Serialize(0.10m).ShouldBe("0.10"); // Trailing zero is preserved.
-        Serialize(decimal.MaxValue).ShouldBe("79228162514264337593543950335");
-
-        Serialize(double.MinValue).ShouldBe("-1.7976931348623157E+308");
-        Serialize(0d).ShouldBe("0");
-        Serialize(1d).ShouldBe("1");
-        Serialize(0.1d).ShouldBe("0.1");
-        Serialize(0.10d).ShouldBe("0.1");
-        Serialize(double.MaxValue).ShouldBe("1.7976931348623157E+308");
-
-        Serialize(float.MinValue).ShouldBe("-3.4028235E+38");
-        Serialize(0f).ShouldBe("0");
-        Serialize(1f).ShouldBe("1");
-        Serialize(0.1f).ShouldBe("0.1");
-        Serialize(0.10f).ShouldBe("0.1");
-        Serialize(float.MaxValue).ShouldBe("3.4028235E+38");
-    }
-
     public void ShouldSerializeEnums()
     {
         Serialize((StringSplitOptions?)null)
