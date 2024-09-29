17800a3640ecc2866b9fa1afd099d59c4446f219
Patrick Lioi <patrick.lioi@gmail.com>
2024-09-29 14:38:32 -0500

    Move bool tests to their own file.

---
+++ src/Fixie.Assertions.Tests/BoolTests.cs
@@ -0,0 +1,44 @@
+﻿namespace Tests;
+
+class BoolTests
+{
+    public void ShouldSerializeBools()
+    {
+        Serialize(true).ShouldBe("true");
+        Serialize(false).ShouldBe("false");
+
+        Serialize((bool?)null).ShouldBe("null");        
+        Serialize((bool?)true).ShouldBe("true");
+        Serialize((bool?)false).ShouldBe("false");
+    }
+
+    public void ShouldAssertBools()
+    {
+        true.ShouldBe(true);
+        false.ShouldBe(false);
+
+        Contradiction(true, x => x.ShouldBe(false), "x should be false but was true");
+        Contradiction(false, x => x.ShouldBe(true), "x should be true but was false");
+    }
+
+    public void ShouldAssertNullableBools()
+    {
+        bool? nullableBool = null;
+
+        nullableBool.ShouldBe(null);
+        Contradiction(nullableBool, x => x.ShouldBe(false), "x should be false but was null");
+        Contradiction(nullableBool, x => x.ShouldBe(true), "x should be true but was null");
+
+        nullableBool = false;
+
+        Contradiction(nullableBool, x => x.ShouldBe(null), "x should be null but was false");
+        nullableBool.ShouldBe(false);
+        Contradiction(nullableBool, x => x.ShouldBe(true), "x should be true but was false");
+
+        nullableBool = true;
+
+        Contradiction(nullableBool, x => x.ShouldBe(null), "x should be null but was true");
+        Contradiction(nullableBool, x => x.ShouldBe(false), "x should be false but was true");
+        nullableBool.ShouldBe(true);
+    }
+}
--- src/Fixie.Assertions.Tests/PrimitiveAssertionTests.cs
+++ src/Fixie.Assertions.Tests/PrimitiveAssertionTests.cs
@@ -2,33 +2,6 @@
 
 class PrimitiveAssertionTests
 {
-    public void ShouldAssertBools()
-    {
-        true.ShouldBe(true);
-        false.ShouldBe(false);
-
-        Contradiction(true, x => x.ShouldBe(false), "x should be false but was true");
-        Contradiction(false, x => x.ShouldBe(true), "x should be true but was false");
-
-        bool? nullableBool = null;
-
-        nullableBool.ShouldBe(null);
-        Contradiction(nullableBool, x => x.ShouldBe(false), "x should be false but was null");
-        Contradiction(nullableBool, x => x.ShouldBe(true), "x should be true but was null");
-
-        nullableBool = false;
-
-        Contradiction(nullableBool, x => x.ShouldBe(null), "x should be null but was false");
-        nullableBool.ShouldBe(false);
-        Contradiction(nullableBool, x => x.ShouldBe(true), "x should be true but was false");
-
-        nullableBool = true;
-
-        Contradiction(nullableBool, x => x.ShouldBe(null), "x should be null but was true");
-        Contradiction(nullableBool, x => x.ShouldBe(false), "x should be false but was true");
-        nullableBool.ShouldBe(true);
-    }
-
     public void ShouldAssertIntegralNumbers()
     {
         sbyte.MinValue.ShouldBe(sbyte.MinValue);
