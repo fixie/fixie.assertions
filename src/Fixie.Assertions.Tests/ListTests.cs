66b9fe061f0a6ea0fd44f34bbdbe9c69566a6d0d
Patrick Lioi <patrick.lioi@gmail.com>
2024-09-29 17:13:25 -0500

    Improve test coverage of list serialization for variations on the type of the object being serialized.

--- src/Fixie.Assertions.Tests/ListTests.cs
+++ src/Fixie.Assertions.Tests/ListTests.cs
@@ -1,42 +1,102 @@
-﻿namespace Tests;
+﻿using System.Collections;
+
+namespace Tests;
 
 class ListTests
 {
     public void ShouldSerializeLists()
     {
-        Serialize((int[]?)null)
-            .ShouldBe("null");
-
-        Serialize((int[])[])
-            .ShouldBe("[]");
-
-        Serialize((int[])[1, 2, 3])
-            .ShouldBe("""
+        var list123 = """
                       [
                         1,
                         2,
                         3
                       ]
+                      """;
+
+        Serialize((int[]?)null).ShouldBe("null");
+        Serialize((IEnumerable<bool>?)null).ShouldBe("null");
+        Serialize((Custom?)null).ShouldBe("null");
+        Serialize((ICustom?)null).ShouldBe("null");
+
+        Serialize((int[])[]).ShouldBe("[]");
+        Serialize((int[])[1, 2, 3]).ShouldBe(list123);
+
+        Serialize((int[][])[]).ShouldBe("[]");
+        Serialize((int[][])[[1 , 2], [3 , 4, 5]])
+            .ShouldBe("""
+                      [
+                        [
+                          1,
+                          2
+                        ],
+                        [
+                          3,
+                          4,
+                          5
+                        ]
+                      ]
                       """);
+
+        Serialize(new Custom(0)).ShouldBe("[]");
+        Serialize(new Custom(1))
+            .ShouldBe("""
+                      [
+                        1
+                      ]
+                      """);
+        Serialize(new Custom(3)).ShouldBe(list123);
+        Serialize((ICustom)new Custom(3)).ShouldBe(list123);
 
         Serialize((IEnumerable<int>)[1, 2, 3])
-            .ShouldBe("""
-                      [
-                        1,
-                        2,
-                        3
-                      ]
-                      """);
+            .ShouldBe(list123);
 
-        //TODO: The indentation of lists became incorrect once the contained item type, in this case
-        //      strings, started to be written using WriteRawValue. Apparently the preceding expected
-        //      whitespace gets skipped as a consequence of WriteRawValue.
-        Serialize((List<string>)["ABC", "123"])
+        Serialize((List<string[]>)[["ABC", "123"], ["DEF", "456"]])
             .ShouldBe("""
                       [
-                        "ABC",
-                        "123"
+                        [
+                          "ABC",
+                          "123"
+                        ],
+                        [
+                          "DEF",
+                          "456"
+                        ]
                       ]
                       """);
+    }
+
+    class Custom(byte size) : ICustom
+    {
+        public IEnumerator<int> GetEnumerator()
+            => new Enumerator(size);
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        class Enumerator(byte size) : IEnumerator<int>
+        {
+            int count = 0;
+
+            public int Current => count;
+
+            object IEnumerator.Current => Current;
+
+            public bool MoveNext()
+            {
+                count++;
+
+                return count <= size;
+            }
+
+            public void Reset()
+                => count = 0;
+
+            public void Dispose() { }
+        }
+    }
+
+    interface ICustom : IEnumerable<int>
+    {
     }
 }
