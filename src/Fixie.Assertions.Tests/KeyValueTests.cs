c112fe725d0de9fc6a13a199521b4ca3e6d3a7cd
Patrick Lioi <patrick.lioi@gmail.com>
2024-09-30 10:38:33 -0500

    Improve test coverage of key/value pairs equality assertions and structural equality assertions.

--- src/Fixie.Assertions.Tests/KeyValueTests.cs
+++ src/Fixie.Assertions.Tests/KeyValueTests.cs
@@ -4,7 +4,7 @@ namespace Tests;
 
 class KeyValueTests
 {
-    public void ShouldSerializeDictionaries()
+    public void ShouldSerializeKeyValuePairs()
     {
         var dictionary123 = """
                             {
@@ -13,11 +13,6 @@ class KeyValueTests
                               [3] = "***"
                             }
                             """;
-
-        Serialize((Dictionary<string, object>?)null).ShouldBe("null");
-        Serialize((IEnumerable<KeyValuePair<int, string>>?)null).ShouldBe("null");
-        Serialize((Custom?)null).ShouldBe("null");
-        Serialize((ICustom?)null).ShouldBe("null");
 
         Serialize((Dictionary<string, string>)[]).ShouldBe("{}");
         Serialize(new Dictionary<string, string>
@@ -85,6 +80,222 @@ class KeyValueTests
                       }
                     }
                     """);
+
+        Serialize((IEnumerable<KeyValuePair<int, string>>?)null).ShouldBe("null");
+        Serialize((Custom?)null).ShouldBe("null");
+        Serialize((ICustom?)null).ShouldBe("null");
+        Serialize((Dictionary<string, object>?)null).ShouldBe("null");
+        Serialize((Dictionary<string, object>?)[]).ShouldBe("{}");
+    }
+
+    public void ShouldAssertKeyValuePairs()
+    {
+        Dictionary<int, string> empty = [];
+        Dictionary<int, string> emptyNewlyAllocated = [];
+        Dictionary<int, string> unsorted = new()
+        {
+            [1] = "*",
+            [2] = "**",
+            [3] = "***"
+        };
+        SortedDictionary<int, string> sorted = new()
+        {
+            [1] = "*",
+            [2] = "**",
+            [3] = "***"
+        };;
+        
+        empty.ShouldBe(empty);
+
+        unsorted.ShouldBe(unsorted);
+        sorted.ShouldBe(sorted);
+
+        Contradiction(empty, x => x.ShouldBe(new() { [1] = "*", [2] = "**", [3] = "***" }),
+            """
+            x should be
+            
+                {
+                  [1] = "*",
+                  [2] = "**",
+                  [3] = "***"
+                }
+
+            but was
+            
+                {}
+            """);
+
+        Contradiction(empty, x => x.ShouldBe(emptyNewlyAllocated),
+            """
+            x should be
+            
+                {}
+
+            but was
+            
+                {}
+            
+            These serialized values are identical. Did you mean to perform a structural comparison with `ShouldMatch` instead?
+            """);
+        empty.ShouldMatch(emptyNewlyAllocated);
+
+        Contradiction(unsorted, x => x.ShouldBe(new() { [1] = "*", [2] = "**", [3] = "***" }),
+            """
+            x should be
+            
+                {
+                  [1] = "*",
+                  [2] = "**",
+                  [3] = "***"
+                }
+
+            but was
+            
+                {
+                  [1] = "*",
+                  [2] = "**",
+                  [3] = "***"
+                }
+
+            These serialized values are identical. Did you mean to perform a structural comparison with `ShouldMatch` instead?
+            """);
+        unsorted.ShouldMatch(new() { [1] = "*", [2] = "**", [3] = "***" });
+
+        Contradiction(sorted, x => x.ShouldBe(new() { [1] = "*", [2] = "**", [3] = "***" }),
+            """
+            x should be
+            
+                {
+                  [1] = "*",
+                  [2] = "**",
+                  [3] = "***"
+                }
+
+            but was
+            
+                {
+                  [1] = "*",
+                  [2] = "**",
+                  [3] = "***"
+                }
+
+            These serialized values are identical. Did you mean to perform a structural comparison with `ShouldMatch` instead?
+            """);
+        sorted.ShouldMatch(new() { [1] = "*", [2] = "**", [3] = "***" });
+        ((IDictionary<int, string>)sorted).ShouldMatch(unsorted);
+        ((IDictionary<int, string>)unsorted).ShouldMatch(sorted);
+
+        Dictionary<string, Type> intBool = new()
+        {
+            ["int"] = typeof(int),
+            ["bool"] = typeof(bool)
+        };
+        Dictionary<string, Type> intBoolString = new()
+        {
+            ["int"] = typeof(int),
+            ["bool"] = typeof(bool),
+            ["string"] = typeof(string)
+        };;
+
+        intBool.ShouldBe(intBool);
+        intBoolString.ShouldBe(intBoolString);
+
+        Contradiction(intBool, x => x.ShouldBe(new() { ["int"] = typeof(int), ["bool"] = typeof(bool) }),
+            """
+            x should be
+
+                {
+                  ["int"] = typeof(int),
+                  ["bool"] = typeof(bool)
+                }
+            
+            but was
+
+                {
+                  ["int"] = typeof(int),
+                  ["bool"] = typeof(bool)
+                }
+
+            These serialized values are identical. Did you mean to perform a structural comparison with `ShouldMatch` instead?
+            """);
+        intBool.ShouldMatch(new() { ["int"] = typeof(int), ["bool"] = typeof(bool) });
+
+        Contradiction(intBool, x => x.ShouldMatch(intBoolString),
+            """
+            x should match
+
+                {
+                  ["int"] = typeof(int),
+                  ["bool"] = typeof(bool),
+                  ["string"] = typeof(string)
+                }
+            
+            but was
+
+                {
+                  ["int"] = typeof(int),
+                  ["bool"] = typeof(bool)
+                }
+            """);
+
+        Dictionary<int, string>? nullablePairs = null;
+        nullablePairs.ShouldBe(null);
+        nullablePairs.ShouldMatch(null);
+        Contradiction(nullablePairs, x => x.ShouldBe([]),
+            """
+            x should be
+            
+                {}
+            
+            but was
+            
+                null
+            """);
+        Contradiction(nullablePairs, x => x.ShouldMatch([]),
+            """
+            x should match
+            
+                {}
+            
+            but was
+            
+                null
+            """);
+        
+        nullablePairs = [];
+        Contradiction(nullablePairs, x => x.ShouldBe(null),
+            """
+            x should be
+            
+                null
+            
+            but was
+            
+                {}
+            """);
+        Contradiction(nullablePairs, x => x.ShouldMatch(null),
+            """
+            x should match
+            
+                null
+            
+            but was
+            
+                {}
+            """);
+        Contradiction(nullablePairs, x => x.ShouldBe([]),
+            """
+            x should be
+            
+                {}
+            
+            but was
+            
+                {}
+            
+            These serialized values are identical. Did you mean to perform a structural comparison with `ShouldMatch` instead?
+            """);
+        nullablePairs.ShouldMatch([]);
     }
 
     class Custom(byte size) : ICustom
