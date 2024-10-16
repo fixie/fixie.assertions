062a6d696090ddf25a490462c3b6f37d784abbdb
Patrick Lioi <patrick.lioi@gmail.com>
2024-10-16 17:46:18 -0500

    Apply primary constructor to trivial class.

--- src/Fixie.Assertions/SerializationDepthException.cs
+++ src/Fixie.Assertions/SerializationDepthException.cs
@@ -2,14 +2,10 @@
 
 namespace Fixie.Assertions;
 
-public class SerializationDepthException : Exception
+public class SerializationDepthException(string interruptedOutput)
+    : Exception(Introduction + NewLine + NewLine + interruptedOutput)
 {
     const string Introduction =
         "A value could not be serialized because its object graph is too deep. " +
         "Below is the start of the message that was interrupted:";
-
-    public SerializationDepthException(string interruptedOutput)
-        : base(Introduction + NewLine + NewLine + interruptedOutput)
-    {
-    }
 }
