8e8f4298c5c3b4b77f13e57784ab4ab4dde2b5ab
Patrick Lioi <patrick.lioi@gmail.com>
2024-11-13 13:56:20 -0600

    Remove the "terminal logger" command line flag, as it is the default behavior starting in the .NET 9 SDK.

--- build.ps1
+++ build.ps1
@@ -12,7 +12,7 @@ if (test-path packages) { remove-item packages -Recurse }
 
 step { dotnet tool restore }
 step { dotnet clean src -c Release --nologo -v minimal }
-step { dotnet build src -c Release --nologo --tl }
+step { dotnet build src -c Release --nologo }
 step { dotnet fixie *.Tests -c Release --no-build }
 
 if ($pack) {
